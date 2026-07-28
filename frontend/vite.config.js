715a89ca5e6b3a3fe7856f5927b5f76e32ec8c29
yangdongnan <a101080285@gmail.com>
2026-07-28 14:11:14 +0800

    修复前端资源路径，添加nginx注释

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/stock-analysis/',
   plugins: [vue()],
   server: {
     proxy: {
-      '/api': {
+      '/stock-analysis/api': {
         target: 'http://localhost:5080',
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/stock-analysis/, ''),
       },
     },
   },
